7ede13fa00cd940fbaca61d8285f061cf64f483e
Maori <maori_sayoud--leclercq@ens.univ-artois.fr>
2024-04-05 16:37:21 +0200

    register, login et logout finis, #3, #4, #5

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -25,10 +26,7 @@ class User extends Authenticatable
 
     public $timestamps = false;
 
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -50,7 +48,15 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function roles(){
-        return $this->belongsToMany(Role::class);
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+
+        ];
     }
 }
