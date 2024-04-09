bd6ae7aacf9641190c6b4ab53e90451e28d49462
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2024-04-09 11:22:39 +0200

    :memo: Ajout de la doc auth

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use OpenApi\Attributes\OpenApi;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 use OpenApi\Attributes as OA;
 
